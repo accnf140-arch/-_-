33e68c63332491638e4d1e07aab2f6bb8552d459
¢υℓѕσя ѕσя <accnf140@gmail.com>
2026-07-25 18:45:41 +0530

    Update print statement from 'Hello' to 'Goodbye'

---
+++ strings/helpers.py
@@ -0,0 +1,426 @@
+HELP_1 = """<b><u>ᴀᴅᴍɪɴ ᴄᴏᴍᴍᴀɴᴅs :</u></b>
+
+ᴊᴜsᴛ ᴀᴅᴅ <b>ᴄ</b> ᴀᴛ ᴛʜᴇ ʙᴇɢɪɴɴɪɴɢ ᴏғ ᴄᴏᴍᴍᴀɴᴅs ᴛᴏ ᴜsᴇ ᴛʜᴇᴍ ғᴏʀ ᴄʜᴀɴɴᴇʟs.
+
+• /pause : ᴘᴀᴜsᴇs ᴛʜᴇ ᴄᴜʀʀᴇɴᴛ ᴘʟᴀʏɪɴɢ sᴛʀᴇᴀᴍ.  
+• /resume : ʀᴇsᴜᴍᴇs ᴛʜᴇ ᴘᴀᴜsᴇᴅ sᴛʀᴇᴀᴍ.  
+• /skip : sᴋɪᴘs ᴛʜᴇ ᴄᴜʀʀᴇɴᴛ ᴛʀᴀᴄᴋ ᴀɴᴅ sᴛᴀʀᴛs ᴛʜᴇ ɴᴇxᴛ ᴏɴᴇ.  
+• /end ᴏʀ /stop : ᴄʟᴇᴀʀs ᴛʜᴇ ǫᴜᴇᴜᴇ ᴀɴᴅ sᴛᴏᴘs ᴛʜᴇ sᴛʀᴇᴀᴍ.  
+• /player : ᴏᴘᴇɴs ᴀɴ ɪɴᴛᴇʀᴀᴄᴛɪᴠᴇ ᴘʟᴀʏᴇʀ ᴘᴀɴᴇʟ.  
+• /queue : sʜᴏᴡs ᴛʜᴇ ǫᴜᴇᴜᴇᴅ ᴛʀᴀᴄᴋs.
+"""
+
+HELP_2 = """<b><u>ᴀᴜᴛʜ ᴜsᴇʀs :</u></b>
+
+ᴀᴜᴛʜ ᴜsᴇʀs ᴄᴀɴ ᴜsᴇ ᴀᴅᴍɪɴ ᴄᴏᴍᴍᴀɴᴅs ᴡɪᴛʜᴏᴜᴛ ʜᴀᴠɪɴɢ ᴀᴅᴍɪɴ ʀɪɢʜᴛs ɪɴ ᴛʜᴇ ᴄʜᴀᴛ.
+
+• /auth [ᴜsᴇʀɴᴀᴍᴇ/ɪᴅ] : ᴀᴅᴅs ᴀ ᴜsᴇʀ ᴛᴏ ᴛʜᴇ ᴀᴜᴛʜ ʟɪsᴛ.  
+• /unauth [ᴜsᴇʀɴᴀᴍᴇ/ɪᴅ] : ʀᴇᴍᴏᴠᴇs ᴀ ᴜsᴇʀ ғʀᴏᴍ ᴛʜᴇ ᴀᴜᴛʜ ʟɪsᴛ.  
+• /authusers : sʜᴏᴡs ᴛʜᴇ ʟɪsᴛ ᴏғ ᴀᴜᴛʜᴏʀɪᴢᴇᴅ ᴜsᴇʀs.
+"""
+
+HELP_3 = """<b><u>ʙʀᴏᴀᴅᴄᴀsᴛ ғᴇᴀᴛᴜʀᴇ :</u></b> [sᴜᴅᴏ ᴏɴʟʏ]
+
+• /broadcast [ᴍᴇssᴀɢᴇ/ʀᴇᴩʟʏ] : sᴇɴᴅs ᴀ ʙʀᴏᴀᴅᴄᴀsᴛ ᴛᴏ ᴀʟʟ sᴇʀᴠᴇᴅ ᴄʜᴀᴛs.
+
+<b>ᴍᴏᴅᴇs :</b>  
+- <b>-pin</b> : ᴘɪɴs ᴛʜᴇ ᴍᴇssᴀɢᴇ.  
+- <b>-pinloud</b> : ᴘɪɴs ᴀɴᴅ sᴇɴᴅs ɴᴏᴛɪғɪᴄᴀᴛɪᴏɴ.  
+- <b>-user</b> : sᴇɴᴅs ᴛᴏ ᴜsᴇʀs ᴡʜᴏ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ.  
+- <b>-assistant</b> : sᴇɴᴅs ғʀᴏᴍ ᴛʜᴇ ᴀssɪsᴛᴀɴᴛ ᴀᴄᴄᴏᴜɴᴛ.  
+- <b>-nobot</b> : ᴅɪsᴀʙʟᴇs ʙᴏᴛ sᴇɴᴅɪɴɢ.
+
+<b>ᴇxᴀᴍᴩʟᴇ :</b>  
+<code>/broadcast -user -assistant -pin ᴛᴇsᴛɪɴɢ ʙʀᴏᴀᴅᴄᴀsᴛ</code>
+"""
+
+HELP_4 = """<b><u>ᴄʜᴀᴛ ʙʟᴀᴄᴋʟɪsᴛ :</u></b> [sᴜᴅᴏ ᴏɴʟʏ]
+
+ʀᴇsᴛʀɪᴄᴛ ᴀʙᴜsɪᴠᴇ ᴏʀ sᴘᴀᴍᴍʏ ᴄʜᴀᴛs ғʀᴏᴍ ᴜsɪɴɢ ᴛʜᴇ ʙᴏᴛ.
+
+• /blacklistchat [ɪᴅ] : ʙʟᴀᴄᴋʟɪsᴛs ᴀ ᴄʜᴀᴛ.  
+• /whitelistchat [ɪᴅ] : ʀᴇᴍᴏᴠᴇs ᴀ ᴄʜᴀᴛ ғʀᴏᴍ ʙʟᴀᴄᴋʟɪsᴛ.  
+• /blacklistedchat : sʜᴏᴡs ᴀʟʟ ʙʟᴀᴄᴋʟɪsᴛᴇᴅ ᴄʜᴀᴛs.
+"""
+
+HELP_5 = """<b><u>ʙʟᴏᴄᴋ ᴜsᴇʀs :</u></b> [sᴜᴅᴏ ᴏɴʟʏ]
+
+ʙʟᴏᴄᴋᴇᴅ ᴜsᴇʀs ᴄᴀɴ’ᴛ ᴜsᴇ ʙᴏᴛ ᴄᴏᴍᴍᴀɴᴅs.
+
+• /block [ᴜsᴇʀ/ʀᴇᴩʟʏ] : ʙʟᴏᴄᴋs ᴀ ᴜsᴇʀ.  
+• /unblock [ᴜsᴇʀ/ʀᴇᴩʟʏ] : ᴜɴʙʟᴏᴄᴋs ᴀ ᴜsᴇʀ.  
+• /blockedusers : sʜᴏᴡs ᴀʟʟ ʙʟᴏᴄᴋᴇᴅ ᴜsᴇʀs.
+"""
+
+HELP_6 = """<b><u>ᴄʜᴀɴɴᴇʟ ᴘʟᴀʏ :</u></b>
+
+sᴛʀᴇᴀᴍ ᴀᴜᴅɪᴏ/ᴠɪᴅᴇᴏ ɪɴ ᴀ ᴄʜᴀɴɴᴇʟ.
+
+• /cplay : sᴛᴀʀᴛ ᴀᴜᴅɪᴏ sᴛʀᴇᴀᴍɪɴɢ.  
+• /cvplay : sᴛᴀʀᴛ ᴠɪᴅᴇᴏ sᴛʀᴇᴀᴍɪɴɢ.  
+• /cplayforce or /cvplayforce : ғᴏʀᴄᴇs ᴀ ɴᴇᴡ sᴛʀᴇᴀᴍ.  
+• /channelplay [ᴜsᴇʀɴᴀᴍᴇ/ɪᴅ] ᴏʀ [disable] : ʟɪɴᴋs ᴀ ᴄʜᴀɴɴᴇʟ ᴡɪᴛʜ ᴀ ɢʀᴏᴜᴩ.
+"""
+
+HELP_7 = """<b><u>ɢʟᴏʙᴀʟ ʙᴀɴ :</u></b> [sᴜᴅᴏ ᴏɴʟʏ]
+
+• /gban [ᴜsᴇʀ/ʀᴇᴩʟʏ] : ɢʟᴏʙᴀʟʟʏ ʙᴀɴs ᴀ ᴜsᴇʀ.  
+• /ungban [ᴜsᴇʀ/ʀᴇᴩʟʏ] : ᴜɴʙᴀɴs ᴀ ɢʟᴏʙᴀʟʟʏ ʙᴀɴɴᴇᴅ ᴜsᴇʀ.  
+• /gbannedusers : sʜᴏᴡs ᴛʜᴇ ʟɪsᴛ ᴏғ ɢʟᴏʙᴀʟʟʏ ʙᴀɴɴᴇᴅ ᴜsᴇʀs.
+"""
+
+HELP_8 = """<b><u>ʟᴏᴏᴘ sᴛʀᴇᴀᴍ :</u></b>
+
+ʀᴇᴘᴇᴀᴛ ᴛʜᴇ ᴄᴜʀʀᴇɴᴛ sᴛʀᴇᴀᴍ.
+
+• /loop [enable/disable] : ᴇɴᴀʙʟᴇs ᴏʀ ᴅɪsᴀʙʟᴇs ʟᴏᴏᴘ.  
+• /loop [1–10] : sᴇᴛs ʟᴏᴏᴘ ᴄᴏᴜɴᴛ.
+"""
+
+HELP_9 = """<b><u>ᴍᴀɪɴᴛᴇɴᴀɴᴄᴇ :</u></b> [sᴜᴅᴏ ᴏɴʟʏ]
+
+• /logs : ɢᴇᴛs ʙᴏᴛ ʟᴏɢs.  
+• /logger [enable/disable] : ᴛᴏɢɢʟᴇs ᴀᴄᴛɪᴠɪᴛʏ ʟᴏɢɢɪɴɢ.  
+• /maintenance [enable/disable] : ᴇɴᴀʙʟᴇs ᴏʀ ᴅɪsᴀʙʟᴇs ᴍᴀɪɴᴛᴇɴᴀɴᴄᴇ ᴍᴏᴅᴇ.
+"""
+
+HELP_10 = """<b><u>ᴘɪɴɢ & sᴛᴀᴛs :</u></b>
+
+• /start : sᴛᴀʀᴛs ᴛʜᴇ ʙᴏᴛ.  
+• /help : sʜᴏᴡs ᴛʜᴇ ʜᴇʟᴩ ᴍᴇɴᴜ.  
+• /ping : sʜᴏᴡs ʙᴏᴛ ʟᴀᴛᴇɴᴄʏ ᴀɴᴅ sʏsᴛᴇᴍ sᴛᴀᴛs.  
+• /stats : sʜᴏᴡs ᴏᴠᴇʀᴀʟʟ ʙᴏᴛ sᴛᴀᴛɪsᴛɪᴄs.
+"""
+
+HELP_11 = """<b><u>ᴩʟᴀʏ ᴄᴏᴍᴍᴀɴᴅs :</u></b>
+
+<b>v :</b> ᴠɪᴅᴇᴏ ᴘʟᴀʏ  
+<b>force :</b> ғᴏʀᴄᴇ ᴘʟᴀʏ  
+
+• /play or /vplay : sᴛᴀʀᴛs ᴀᴜᴅɪᴏ/ᴠɪᴅᴇᴏ sᴛʀᴇᴀᴍɪɴɢ.  
+• /playforce or /vplayforce : ғᴏʀᴄᴇs ᴀ ɴᴇᴡ sᴛʀᴇᴀᴍ.
+"""
+
+HELP_12 = """<b><u>sʜᴜғғʟᴇ ǫᴜᴇᴜᴇ :</u></b>
+
+• /shuffle : sʜᴜғғʟᴇs ᴛʜᴇ ǫᴜᴇᴜᴇ.  
+• /queue : sʜᴏᴡs ᴛʜᴇ sʜᴜғғʟᴇᴅ ǫᴜᴇᴜᴇ.
+"""
+
+HELP_13 = """<b><u>sᴇᴇᴋ sᴛʀᴇᴀᴍ :</u></b>
+
+• /seek [sᴇᴄᴏɴᴅs] : ɢᴏᴇs ғᴏʀᴡᴀʀᴅ ɪɴ ᴛʜᴇ sᴛʀᴇᴀᴍ.  
+• /seekback [sᴇᴄᴏɴᴅs] : sᴇᴇᴋs ʙᴀᴄᴋᴡᴀʀᴅ.
+"""
+
+HELP_14 = """<b><u>sᴏɴɢ ᴅᴏᴡɴʟᴏᴀᴅ :</u></b>
+
+• /song [ɴᴀᴍᴇ/ʏᴛ ᴜʀʟ] : ᴅᴏᴡɴʟᴏᴀᴅs ᴀ sᴏɴɢ ɪɴ ᴍᴘ3 ᴏʀ ᴍᴘ4 ғᴏʀᴍᴀᴛ.
+"""
+
+HELP_15 = """<b><u>sᴘᴇᴇᴅ ᴄᴏᴍᴍᴀɴᴅs :</u></b> [ᴀᴅᴍɪɴ ᴏɴʟʏ]
+
+ᴄᴏɴᴛʀᴏʟ ᴛʜᴇ ᴘʟᴀʏʙᴀᴄᴋ sᴘᴇᴇᴅ ᴏғ ᴛʜᴇ sᴛʀᴇᴀᴍ.
+
+• /speed or /playback : ᴀᴅᴊᴜsᴛs ᴀᴜᴅɪᴏ sᴘᴇᴇᴅ ɪɴ ɢʀᴏᴜᴩs.  
+• /cspeed or /cplayback : ᴀᴅᴊᴜsᴛs ᴀᴜᴅɪᴏ sᴘᴇᴇᴅ ɪɴ ᴄʜᴀɴɴᴇʟs.
+"""
+
+HELP_16 = """<b><u>ᴄʜᴀᴛғɪɢʜᴛ ɢᴀᴍᴇs :</u></b>
+
+Fun word / emoji / flag guessing mini-games with a global leaderboard.
+
+• /wordgame or /cfword : sᴛᴀʀᴛs ᴀ ᴡᴏʀᴅ ɢᴜᴇssɪɴɢ ɢᴀᴍᴇ.
+• /emojigame or /cfemoji : sᴛᴀʀᴛs ᴀɴ ᴇᴍᴏᴊɪ ɢᴜᴇssɪɴɢ ɢᴀᴍᴇ.
+• /flaggame or /cfflag : sᴛᴀʀᴛs ᴀ ғʟᴀɢ ɢᴜᴇssɪɴɢ ɢᴀᴍᴇ.
+• /wordleaderboard or /gametop or /cflb : sʜᴏᴡs ᴛʜᴇ ɢʟᴏʙᴀʟ ʟᴇᴀᴅᴇʀʙᴏᴀʀᴅ.
+
+Games also start automatically after 5 minutes of chat inactivity to keep the group active.
+"""
+
+HELP_17 = """<b><u>ɢɪᴛʜᴜʙ ᴍᴀɴᴀɢᴇᴍᴇɴᴛ :</u></b>
+
+Convert repos to ZIP, push ZIPs to your repo, and manage your GitHub account — all from Telegram.
+
+• /setghtoken [token] : sᴀᴠᴇs ʏᴏᴜʀ ɢɪᴛʜᴜʙ ᴛᴏᴋᴇɴ (ᴅᴍ ᴏɴʟʏ).
+• /mytoken : ᴄʜᴇᴄᴋs ɪғ ᴀ ᴛᴏᴋᴇɴ ɪs sᴀᴠᴇᴅ.
+• /removetoken : ʀᴇᴍᴏᴠᴇs ʏᴏᴜʀ sᴀᴠᴇᴅ ᴛᴏᴋᴇɴ.
+• /repo2zip [owner/repo] [branch] : ᴅᴏᴡɴʟᴏᴀᴅs ᴀɴʏ ᴘᴜʙʟɪᴄ ʀᴇᴘᴏ ᴀs ᴀ ᴢɪᴘ.
+• /pushzip [owner/repo] [branch] : (ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴢɪᴘ) ᴘᴜsʜᴇs ɪᴛs ᴄᴏɴᴛᴇɴᴛs ᴛᴏ ʏᴏᴜʀ ʀᴇᴘᴏ.
+• /myrepos : ʟɪsᴛs ʏᴏᴜʀ ʀᴇᴘᴏsɪᴛᴏʀɪᴇs.
+• /creategh [name] : ᴄʀᴇᴀᴛᴇs ᴀ ɴᴇᴡ ʀᴇᴘᴏsɪᴛᴏʀʏ.
+• /ghinfo [owner/repo] : sʜᴏᴡs ʀᴇᴘᴏ ɪɴғᴏ.
+• /ghfiles [owner/repo] [path] : ʟɪsᴛs ғɪʟᴇs ɪɴ ᴀ ʀᴇᴘᴏ ᴘᴀᴛʜ.
+• /delghfile [owner/repo] [path] : ᴅᴇʟᴇᴛᴇs ᴀ ғɪʟᴇ ғʀᴏᴍ ᴀ ʀᴇᴘᴏ.
+• /delgh [owner/repo] : ᴅᴇʟᴇᴛᴇs ᴀ ʀᴇᴘᴏsɪᴛᴏʀʏ.
+"""
+
+HELP_18 = """<b><u>ᴄʜᴀᴛʙᴏᴛ :</u></b>
+
+A MongoDB-powered auto-reply chatbot — no external AI API used.
+
+• /chatbot on/off : ᴇɴᴀʙʟᴇs ᴏʀ ᴅɪsᴀʙʟᴇs ᴀᴜᴛᴏ-ʀᴇᴘʟʏ ɪɴ ᴛʜᴇ ᴄʜᴀᴛ (ᴀᴅᴍɪɴ ᴏɴʟʏ).
+• /teach [keyword] | [reply] : ᴛᴇᴀᴄʜᴇs ᴛʜᴇ ʙᴏᴛ ᴀ ɴᴇᴡ ʀᴇᴘʟʏ (ᴀᴅᴍɪɴ ᴏɴʟʏ).
+• /unlearn [keyword] : ʀᴇᴍᴏᴠᴇs ᴀ ᴛᴀᴜɢʜᴛ ʀᴇᴘʟʏ (ᴀᴅᴍɪɴ ᴏɴʟʏ).
+• /learned : ʟɪsᴛs ᴇᴠᴇʀʏᴛʜɪɴɢ ʟᴇᴀʀɴᴇᴅ ɪɴ ᴛʜᴇ ᴄʜᴀᴛ.
+"""
+
+HELP_20 = """🔑 <b><u>sᴛʀɪɴɢ sᴇssɪᴏɴ ɢᴇɴᴇʀᴀᴛᴏʀ :</u></b>
+
+ɢᴇɴᴇʀᴀᴛᴇ ʏᴏᴜʀ sᴇssɪᴏɴ sᴛʀɪɴɢ sᴀꜰᴇʟʏ ᴅɪʀᴇᴄᴛʟʏ ꜰʀᴏᴍ ᴛᴇʟᴇɢʀᴀᴍ ᴅᴍ.
+
+• /genstring — sᴛᴀʀᴛ ɪɴᴛᴇʀᴀᴄᴛɪᴠᴇ sᴛʀɪɴɢ ɢᴇɴᴇʀᴀᴛᴏʀ
+
+⭐ <b>Pyrogram v1</b> — ʟᴇɢᴀᴄʏ sᴛʀɪɴɢ ꜰᴏʀᴍᴀᴛ
+✨ <b>Pyrogram v2</b> — ᴍᴏᴅᴇʀɴ sᴛʀɪɴɢ ꜰᴏʀᴍᴀᴛ
+⚡ <b>Telethon</b>    — ᴛᴇʟᴇᴛʜᴏɴ sᴇssɪᴏɴ sᴛʀɪɴɢ
+🤖 <b>Bot Session</b> — ꜰᴏʀ ʙᴏᴛ ᴀᴄᴄᴏᴜɴᴛs
+
+🔒 <i>ʏᴏᴜʀ ᴄʀᴇᴅᴇɴᴛɪᴀʟs ᴀʀᴇ ɴᴇᴠᴇʀ sᴛᴏʀᴇᴅ ᴏɴ ᴛʜᴇ sᴇʀᴠᴇʀ.
+ᴀʟʟ ᴅᴀᴛᴀ ɪs ᴅᴇʟᴇᴛᴇᴅ ɪᴍᴍᴇᴅɪᴀᴛᴇʟʏ ᴀꜰᴛᴇʀ ɢᴇɴᴇʀᴀᴛɪᴏɴ.</i>
+"""
+
+HELP_21 = """⚡ <b><u>ǫᴜɪᴄᴋ ɢʀᴏᴜᴘ ɢᴀᴍᴇs :</u></b>
+
+ᴇᴀsʏ ᴏɴᴇ-ᴄʟɪᴄᴋ ɢᴀᴍᴇs ꜰᴏʀ ɢʀᴏᴜᴘ ᴍᴇᴍʙᴇʀs!
+
+⚡ /taprace  — ᴛᴀᴘ ᴛʜᴇ ʙᴜᴛᴛᴏɴ ꜰɪʀꜱᴛ ᴛᴏ ᴡɪɴ ᴄᴏɪɴꜱ
+🎰 /slots    — ꜱᴘɪɴ ᴛʜᴇ ꜱʟᴏᴛ ᴍᴀᴄʜɪɴᴇ ꜰᴏʀ ᴊᴀᴄᴋᴩᴏᴛ
+🔥 /hotvote  — ɢʀᴏᴜᴩ ᴠᴏᴛᴇꜱ ʜᴏᴛ 🔥 ᴏʀ ɴᴏᴛ ❄️ ᴏɴ ᴀ ᴛᴏᴩɪᴄ
+
+💰 <b>ᴄᴏɪɴ ʀᴇᴡᴀʀᴅꜱ :</b>
+  ⚡ Tap Race  — ꜰɪʀꜱᴛ ᴛᴀᴩ ᴡɪɴꜱ <b>20 ᴄᴏɪɴꜱ</b>
+  🎰 Jackpot   — 3 ᴍᴀᴛᴄʜ = <b>50 ᴄᴏɪɴꜱ</b>  |  2 ᴍᴀᴛᴄʜ = <b>15 ᴄᴏɪɴꜱ</b>
+  🔥 Hot Vote  — ᴍᴀᴊᴏʀɪᴛʏ ꜱɪᴅᴇ ᴇᴀʀɴꜱ <b>10 ᴄᴏɪɴꜱ</b>
+
+<i>ᴀʟʟ ɢᴀᴍᴇꜱ ᴀʀᴇ ɢʀᴏᴜᴩ-ᴏɴʟʏ. ᴏɴᴇ ɢᴀᴍᴇ ᴩᴇʀ ᴄʜᴀᴛ ᴀᴛ ᴀ ᴛɪᴍᴇ.</i>
+"""
+
+HELP_22 = """⚔️ <b><u>ʙᴀɴ & ᴍᴏᴅᴇʀᴀᴛɪᴏɴ ᴄᴏᴍᴍᴀɴᴅs :</u></b>
+
+• /ban [ᴜsᴇʀ/ʀᴇᴘʟʏ] — ʙᴀɴs ᴀ ᴜsᴇʀ ғʀᴏᴍ ᴛʜᴇ ɢʀᴏᴜᴘ.
+• /unban [ᴜsᴇʀ/ʀᴇᴘʟʏ] — ᴜɴʙᴀɴs ᴀ ʙᴀɴɴᴇᴅ ᴜsᴇʀ.
+• /mute [ᴜsᴇʀ/ʀᴇᴘʟʏ] — ᴍᴜᴛᴇs ᴀ ᴜsᴇʀ (ɴᴏ ᴍᴇssᴀɢᴇs).
+• /unmute [ᴜsᴇʀ/ʀᴇᴘʟʏ] — ᴜɴᴍᴜᴛᴇs ᴀ ᴜsᴇʀ.
+• /tmute [ᴜsᴇʀ] [Xs/m/h/d] — ᴛᴇᴍᴘᴏʀᴀʀɪʟʏ ᴍᴜᴛᴇs ᴀ ᴜsᴇʀ.
+• /kick [ᴜsᴇʀ/ʀᴇᴘʟʏ] — ᴋɪᴄᴋs ᴀ ᴜsᴇʀ ᴏᴜᴛ ᴏғ ᴛʜᴇ ɢʀᴏᴜᴘ.
+• /promote [ᴜsᴇʀ/ʀᴇᴘʟʏ] — ᴘʀᴏᴍᴏᴛᴇs ᴀ ᴜsᴇʀ ᴀs ᴀᴅᴍɪɴ.
+• /demote [ᴜsᴇʀ/ʀᴇᴘʟʏ] — ᴅᴇᴍᴏᴛᴇs ᴀɴ ᴀᴅᴍɪɴ.
+• /warn [ᴜsᴇʀ/ʀᴇᴘʟʏ] — ɢɪᴠᴇs ᴀ ᴡᴀʀɴɪɴɢ (3 ᴡᴀʀɴs = ᴀᴜᴛᴏ-ʙᴀɴ).
+• /rmwarn [ᴜsᴇʀ/ʀᴇᴘʟʏ] — ʀᴇᴍᴏᴠᴇs ᴡᴀʀɴɪɴɢs.
+• /del — ᴅᴇʟᴇᴛᴇs ᴀ ʀᴇᴘʟɪᴇᴅ ᴍᴇssᴀɢᴇ.
+• /purge — ᴘᴜʀɢᴇs ᴍᴇssᴀɢᴇs ᴜᴘ ᴛᴏ ʀᴇᴘʟʏ.
+
+<i>ᴀʟʟ ᴄᴏᴍᴍᴀɴᴅs ʀᴇǫᴜɪʀᴇ ᴀᴅᴍɪɴ ʀɪɢʜᴛs.</i>
+"""
+
+HELP_23 = """📌 <b><u>ᴛᴀɢ ᴄᴏᴍᴍᴀɴᴅs :</u></b>
+
+• /tagall ᴏʀ /all — ᴛᴀɢs ᴀʟʟ ᴍᴇᴍʙᴇʀs ɪɴ ᴛʜᴇ ɢʀᴏᴜᴘ.
+• /admintag ᴏʀ /admins — ᴛᴀɢs ᴀʟʟ ᴀᴅᴍɪɴs.
+• /hitag — ᴛᴀɢs ᴍᴇᴍʙᴇʀs ᴏɴᴇ-ʙʏ-ᴏɴᴇ ᴡɪᴛʜ ᴀ ᴅᴇʟᴀʏ.
+• /lifetag — ᴛᴀɢs ᴀʟʟ ᴍᴇᴍʙᴇʀs ᴡɪᴛʜ ᴄᴏɴᴛɪɴᴜᴏᴜs ᴛᴀɢɢɪɴɢ.
+• /gmtag — ᴛᴀɢs ᴇᴠᴇʀʏ ᴍᴇᴍʙᴇʀ ɪɴᴅɪᴠɪᴅᴜᴀʟʟʏ.
+• /gntag — sɪʟᴇɴᴛ ᴛᴀɢ ᴡɪᴛʜᴏᴜᴛ ɴᴏᴛɪғɪᴄᴀᴛɪᴏɴ.
+• /cancel ᴏʀ /histop — sᴛᴏᴘs ᴀɴʏ ᴏɴɢᴏɪɴɢ ᴛᴀɢ ᴏᴘᴇʀᴀᴛɪᴏɴ.
+• /mention [ᴍᴇssᴀɢᴇ] — ᴍᴇɴᴛɪᴏɴs ᴀʟʟ ᴍᴇᴍʙᴇʀs ᴡɪᴛʜ ᴀ ᴄᴜsᴛᴏᴍ ᴍᴇssᴀɢᴇ.
+
+<i>ᴀᴅᴍɪɴ ʀɪɢʜᴛs ʀᴇǫᴜɪʀᴇᴅ.</i>
+"""
+
+HELP_24 = """👋 <b><u>ᴡᴇʟᴄᴏᴍᴇ & ɴɪɢʜᴛᴍᴏᴅᴇ :</u></b>
+
+<b>ᴡᴇʟᴄᴏᴍᴇ :</b>
+• /welcome on — ᴇɴᴀʙʟᴇs ᴡᴇʟᴄᴏᴍᴇ ᴍᴇssᴀɢᴇ ɪɴ ᴛʜᴇ ɢʀᴏᴜᴘ.
+• /welcome off — ᴅɪsᴀʙʟᴇs ᴡᴇʟᴄᴏᴍᴇ ᴍᴇssᴀɢᴇ.
+• /welcome [ᴛᴇxᴛ] — sᴇᴛs ᴀ ᴄᴜsᴛᴏᴍ ᴡᴇʟᴄᴏᴍᴇ ᴍᴇssᴀɢᴇ.
+
+<b>ɴɪɢʜᴛᴍᴏᴅᴇ :</b>
+• /nightmode on — ʟᴏᴄᴋs ᴛʜᴇ ɢʀᴏᴜᴘ ᴅᴜʀɪɴɢ ᴀ sᴇᴛ ɴɪɢʜᴛ ᴡɪɴᴅᴏᴡ.
+• /nightmode off — ᴅɪsᴀʙʟᴇs ɴɪɢʜᴛᴍᴏᴅᴇ.
+• /nightmode — sʜᴏᴡs ᴄᴜʀʀᴇɴᴛ ɴɪɢʜᴛᴍᴏᴅᴇ sᴛᴀᴛᴜs.
+
+<i>ᴀᴅᴍɪɴ ʀɪɢʜᴛs ʀᴇǫᴜɪʀᴇᴅ.</i>
+"""
+
+HELP_25 = """💑 <b><u>ᴄᴏᴜᴘʟᴇ ᴏғ ᴛʜᴇ ᴅᴀʏ :</u></b>
+
+• /couple — sᴇʟᴇᴄᴛs ᴛᴡᴏ ʀᴀɴᴅᴏᴍ ᴍᴇᴍʙᴇʀs ᴀs ᴛᴏᴅᴀʏ's ᴄᴏᴜᴘʟᴇ ❤️
+• /couples — sᴀᴍᴇ ᴀs /couple, sʜᴏᴡs ᴀ ғʀᴀᴍᴇᴅ ᴘʜᴏᴛᴏ ᴡɪᴛʜ ʙᴏᴛʜ ᴘʀᴏғɪʟᴇ ᴘɪᴄs 🖼️
+
+📅 ᴀ ɴᴇᴡ ᴄᴏᴜᴘʟᴇ ɪs ᴄʜᴏsᴇɴ ᴇᴠᴇʀʏ ᴅᴀʏ.
+"""
+
+HELP_26 = """ℹ️ <b><u>ᴜsᴇʀ ɪɴғᴏ :</u></b>
+
+• /info [ᴜsᴇʀ/ʀᴇᴘʟʏ] — sʜᴏᴡs ᴅᴇᴛᴀɪʟᴇᴅ ɪɴғᴏ ᴀʙᴏᴜᴛ ᴀ ᴜsᴇʀ (ɪᴅ, ɴᴀᴍᴇ, ᴅᴘ, ᴅᴄ ᴇᴛᴄ).
+• /id — sʜᴏᴡs ʏᴏᴜʀ ᴜsᴇʀ ɪᴅ ᴏʀ ᴛʜᴇ ᴄʜᴀᴛ ɪᴅ.
+• /whois [ᴜsᴇʀ/ʀᴇᴘʟʏ] — ᴀʟɪᴀs ғᴏʀ /info.
+"""
+
+HELP_27 = """⚡ <b><u>ᴛʀᴜᴛʜ & ᴅᴀʀᴇ :</u></b>
+
+• /truth — sᴇɴᴅs ᴀ ʀᴀɴᴅᴏᴍ ᴛʀᴜᴛʜ ǫᴜᴇsᴛɪᴏɴ.
+• /dare — sᴇɴᴅs ᴀ ʀᴀɴᴅᴏᴍ ᴅᴀʀᴇ ᴄʜᴀʟʟᴇɴɢᴇ.
+
+ᴘʟᴀʏ ɪɴ ɢʀᴏᴜᴘs ᴛᴏ ᴋᴇᴇᴘ ᴛʜᴇ ᴄʜᴀᴛ ᴀᴄᴛɪᴠᴇ! 🎲
+"""
+
+HELP_28 = """📋 <b><u>ɴᴏᴛᴇs & ғɪʟᴛᴇʀs :</u></b>
+
+<b>ɴᴏᴛᴇs :</b>
+• /save [ɴᴀᴍᴇ] [ᴄᴏɴᴛᴇɴᴛ/ʀᴇᴘʟʏ] — sᴀᴠᴇs ᴀ ɴᴏᴛᴇ.
+• /get [ɴᴀᴍᴇ] ᴏʀ #ɴᴀᴍᴇ — ʀᴇᴛʀɪᴇᴠᴇs ᴀ sᴀᴠᴇᴅ ɴᴏᴛᴇ.
+• /notes — ʟɪsᴛs ᴀʟʟ ɴᴏᴛᴇs ɪɴ ᴛʜᴇ ᴄʜᴀᴛ.
+• /clear [ɴᴀᴍᴇ] — ᴅᴇʟᴇᴛᴇs ᴀ ɴᴏᴛᴇ.
+• /clearall — ᴅᴇʟᴇᴛᴇs ᴀʟʟ ɴᴏᴛᴇs.
+• /privatenotes on/off — sᴇɴᴅs ɴᴏᴛᴇs ɪɴ ᴘᴍ.
+
+<b>ғɪʟᴛᴇʀs :</b>
+• /filter [ᴋᴇʏᴡᴏʀᴅ] [ʀᴇᴘʟʏ] — sᴇᴛs ᴀɴ ᴀᴜᴛᴏ-ʀᴇᴘʟʏ ᴛʀɪɢɢᴇʀ.
+• /filters — ʟɪsᴛs ᴀʟʟ ᴀᴄᴛɪᴠᴇ ғɪʟᴛᴇʀs.
+• /stopfilter [ᴋᴇʏᴡᴏʀᴅ] — ʀᴇᴍᴏᴠᴇs ᴀ ғɪʟᴛᴇʀ.
+• /stopall — ᴄʟᴇᴀʀs ᴀʟʟ ғɪʟᴛᴇʀs.
+"""
+
+HELP_29 = """💤 <b><u>ᴀғᴋ (ᴀᴡᴀʏ ғʀᴏᴍ ᴋᴇʏʙᴏᴀʀᴅ) :</u></b>
+
+• /afk [ʀᴇᴀsᴏɴ] — ᴍᴀʀᴋs ʏᴏᴜ ᴀs ᴀᴡᴀʏ. ᴛʜᴇ ʙᴏᴛ ᴡɪʟʟ ɴᴏᴛɪғʏ ᴀɴʏᴏɴᴇ ᴡʜᴏ ᴍᴇɴᴛɪᴏɴs ʏᴏᴜ.
+
+ʏᴏᴜ ᴀᴜᴛᴏᴍᴀᴛɪᴄᴀʟʟʏ ᴄᴏᴍᴇ ʙᴀᴄᴋ ᴡʜᴇɴ ʏᴏᴜ sᴇɴᴅ ᴀ ᴍᴇssᴀɢᴇ. 🌙
+"""
+
+HELP_30 = """💰 <b><u>ᴄʀʏᴘᴛᴏ & ᴜᴘɪ ᴛᴏᴏʟs :</u></b>
+
+<b>ᴄʀʏᴘᴛᴏ :</b>
+• /ton — sʜᴏᴡs ʟɪᴠᴇ ᴛᴏɴ ᴄᴏɪɴ ᴘʀɪᴄᴇ.
+• /usdt — sʜᴏᴡs ʟɪᴠᴇ ᴜsᴅᴛ ᴘʀɪᴄᴇ.
+• /balance [ᴡᴀʟʟᴇᴛ] — ᴄʜᴇᴄᴋs ᴛᴏɴ ᴡᴀʟʟᴇᴛ ʙᴀʟᴀɴᴄᴇ.
+
+<b>ᴜᴘɪ :</b>
+• /setupi [ᴜᴘɪ-ɪᴅ] — sᴀᴠᴇs ʏᴏᴜʀ ᴜᴘɪ ɪᴅ ᴛᴏ ᴛʜᴇ ʙᴏᴛ.
+• /gen [ᴀᴍᴏᴜɴᴛ] [ɴᴏᴛᴇ] — ɢᴇɴᴇʀᴀᴛᴇs ᴀ ᴜᴘɪ ᴘᴀʏᴍᴇɴᴛ ǫʀ ᴄᴏᴅᴇ.
+"""
+
+HELP_31 = """🔊 <b><u>ᴠᴄ ʟᴏɢɢᴇʀ :</u></b>
+
+ʟᴏɢs ᴠᴏɪᴄᴇ ᴄʜᴀᴛ ᴊᴏɪɴ/ʟᴇᴀᴠᴇ ᴇᴠᴇɴᴛs ᴛᴏ ᴀ ᴅᴇsɪɢɴᴀᴛᴇᴅ ᴄʜᴀᴛ.
+
+• /vclogger on — ᴇɴᴀʙʟᴇs ᴠᴄ ʟᴏɢɢɪɴɢ.
+• /vclogger off — ᴅɪsᴀʙʟᴇs ᴠᴄ ʟᴏɢɢɪɴɢ.
+• /vclogger set [ᴄʜᴀᴛ-ɪᴅ] — sᴇᴛs ᴛʜᴇ ʟᴏɢ ᴅᴇsᴛɪɴᴀᴛɪᴏɴ ᴄʜᴀᴛ.
+• /vclogger status — sʜᴏᴡs ᴄᴜʀʀᴇɴᴛ ᴠᴄ ʟᴏɢɢᴇʀ ᴄᴏɴғɪɢ.
+
+<i>ᴀᴅᴍɪɴ ʀɪɢʜᴛs ʀᴇǫᴜɪʀᴇᴅ.</i>
+"""
+
+HELP_32 = """📢 <b><u>ꜰᴏʀᴄᴇ sᴜʙsᴄʀɪʙᴇ :</u></b>
+
+Require users to join a channel before using bot commands in the group.
+
+• /fsub @channel — set required channel
+• /fsub channel_id — set by channel ID
+• /fsub off — disable force subscribe
+
+⚠️ <i>Bot must be admin in the channel. Admins are exempt from FSub check.</i>
+"""
+
+HELP_33 = """🔔 <b><u>ᴀᴜᴛᴏ ᴀᴘᴘʀᴏᴠᴇ :</u></b>
+
+Automatically approve all join requests to your group.
+
+• /autoapprove on — enable auto approve
+• /autoapprove off — disable auto approve
+• /autoapprove — check current status
+
+⚠️ <i>Bot must have "Add Members" permission in the group.</i>
+"""
+
+HELP_34 = """🛡️ <b><u>ɢᴜᴀʀᴅɪᴀɴ — ᴀɴᴛɪ ꜰʟᴏᴏᴅ :</u></b>
+
+Protect your group from message flooding.
+Users who send 5+ messages in 5 seconds are automatically muted for 5 minutes.
+
+• /guardian on — enable guardian/anti-flood
+• /guardian off — disable guardian
+• /guardian — check status
+
+⚠️ <i>Admins are never muted. Bot must have restrict permissions.</i>
+"""
+
+HELP_35 = """❤️ <b><u>ꜰᴇᴇʟɪɴɢs / ʀᴇᴀᴄᴛɪᴏɴs :</u></b>
+
+Express feelings with anime GIFs! Reply to a user or mention them.
+
+• /hug @user — hug someone 🤗
+• /pat @user — pat someone 🥰
+• /slap @user — slap someone 👋
+• /kiss @user — kiss someone 😘
+• /cuddle @user — cuddle 🫂
+• /wave @user — wave 👋
+• /poke @user — poke 👉
+• /bite @user — bite 😬
+• /punch @user — punch 👊
+• /handshake @user — shake hands 🤝
+• /smile @user — smile 😄
+• /happy — express happiness 😊
+• /sad — express sadness 😢
+• /blush — blush 😊
+• /dance — dance 💃
+"""
+
+HELP_36 = """👬 <b><u>ʙʀᴏᴛʜᴇʀ / sɪsᴛᴇʀ ʀᴇʟᴀᴛɪᴏɴs :</u></b>
+
+Set family relationships with other users.
+
+• /brother @user — set your brother
+• /bro — see your current brother
+• /sister @user — set your sister
+• /sis — see your current sister
+• /family — see all your relations
+• /myrelations — same as /family
+"""
+
+HELP_37 = """🔊 <b><u>ᴛᴛs — ᴛᴇxᴛ ᴛᴏ sᴘᴇᴇᴄʜ :</u></b>
+
+Convert any text to speech in 30+ languages!
+
+• /tts [text] — converts using group language
+• /tts hi नमस्ते — Hindi speech
+• /tts en Hello world — English speech
+• /tts ar مرحبا — Arabic speech
+
+<b>🌍 Common Language Codes:</b>
+<code>hi</code> Hindi · <code>en</code> English · <code>te</code> Telugu
+<code>ta</code> Tamil · <code>ml</code> Malayalam · <code>bn</code> Bengali
+<code>ar</code> Arabic · <code>fr</code> French · <code>de</code> German
+<code>es</code> Spanish · <code>ja</code> Japanese · <code>ko</code> Korean
+<code>ru</code> Russian · <code>tr</code> Turkish · <code>zh</code> Chinese
+<code>ur</code> Urdu · <code>th</code> Thai · <code>vi</code> Vietnamese
+"""
+
+HELP_19 = """🎮 <b><u>ɢʀᴏᴜᴘ ɢᴀᴍᴇs :</u></b>
+
+ᴜsᴇ ᴛʜᴇsᴇ ᴄᴏᴍᴍᴀɴᴅs ᴛᴏ ᴘʟᴀʏ ɪɴᴛᴇʀᴀᴄᴛɪᴠᴇ ɢᴀᴍᴇs ɪɴ ʏᴏᴜʀ ɢʀᴏᴜᴘ!
+
+🎲 <b>ᴅɪᴄᴇ ʙᴀᴛᴛʟᴇ</b>
+• /dicebattle — sᴛᴀʀᴛ ᴀ ɢʀᴏᴜᴘ ᴅɪᴄᴇ ʀᴏʟʟɪɴɢ ʙᴀᴛᴛʟᴇ. ᴇᴠᴇʀʏᴏɴᴇ ᴊᴏɪɴs, ʜɪɢʜᴇsᴛ ʀᴏʟʟ ᴡɪɴs!
+
+💣 <b>ɴᴜᴍʙᴇʀ ʙᴏᴍʙ</b>
+• /numberbomb — ᴘɪᴄᴋ ᴀ ɴᴜᴍʙᴇʀ 1–20. ᴏɴᴇ ɪs ᴛʜᴇ ʙᴏᴍʙ. ᴅᴏɴ'ᴛ ᴇxᴘʟᴏᴅᴇ!
+
+🎡 <b>ʀᴜssɪᴀɴ ʀᴏᴜʟᴇᴛᴛᴇ</b>
+• /roulette — sᴛᴀʀᴛ ᴀ ɢʀᴏᴜᴘ ʀᴏᴜʟᴇᴛᴛᴇ. ᴇᴀᴄʜ ᴘʟᴀʏᴇʀ ᴘᴜʟʟs ᴛʜᴇ ᴛʀɪɢɢᴇʀ ɪɴ ᴛᴜʀɴ.
+
+🏆 <b>ᴛʀɪᴠɪᴀ ʙᴀᴛᴛʟᴇ ʀᴏʏᴀʟᴇ</b>
+• /triviabattle ᴏʀ /battle — sᴛᴀʀᴛ ᴀ 5-ʀᴏᴜɴᴅ ᴍᴜʟᴛɪᴘʟᴀʏᴇʀ ǫᴜɪᴢ. ᴇᴠᴇʀʏᴏɴᴇ ᴊᴏɪɴs, ᴀɴsᴡᴇʀ ғᴀsᴛ!
+• /triviatop — ᴠɪᴇᴡ ᴛʜᴇ ɢʟᴏʙᴀʟ ᴛʀɪᴠɪᴀ ʟᴇᴀᴅᴇʀʙᴏᴀʀᴅ.
+
+🃏 <b>ᴄʜᴀᴛ ᴍɪɴɪ-ɢᴀᴍᴇs</b>
+• /wordgame — ɢᴜᴇss ᴛʜᴇ ᴡᴏʀᴅ (ᴡɪᴛʜ ɪᴍᴀɢᴇ ʜɪɴᴛ).
+• /emojigame — ɢᴜᴇss ᴛʜᴇ ᴇᴍᴏᴊɪ.
+• /flaggame — ɢᴜᴇss ᴛʜᴇ ᴄᴏᴜɴᴛʀʏ ғʀᴏᴍ ɪᴛs ғʟᴀɢ.
+• /gametop — ᴠɪᴇᴡ ᴛʜᴇ ᴡᴏʀᴅɢᴀᴍᴇ ʟᴇᴀᴅᴇʀʙᴏᴀʀᴅ.
+
+<i>ᴀʟʟ ɢᴀᴍᴇs ᴀʀᴇ ɢʀᴏᴜᴘ-ᴏɴʟʏ ᴀɴᴅ ᴡᴏʀᴋ ᴡɪᴛʜ ɪɴʟɪɴᴇ ʙᴜᴛᴛᴏɴs.</i>
+"""
